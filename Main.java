243b5b2c9a2ce3f95f6c5af9e1f83f889e1d7933
Raf <preault.rafael@gmail.com>
2022-09-29 16:02:34 +0200

    ajout modif

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 public class Main{
 
     public static void main(String[] args) {
-        System.out.println("Hello World toto");
+        System.out.println("Hello World");
+
+        Fraction f = new Fraction();
+        assert "Je suis une fraction".equals(f.toString());
     }
 }
